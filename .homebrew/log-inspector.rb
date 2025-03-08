2d722747855bc248e9230706eaacb122293e1759
Anka <runner@Mac-1741429390172.local>
2025-03-08 10:43:53 +0000

    Update formula to version 0.1.1

--- .homebrew/log-inspector.rb
+++ .homebrew/log-inspector.rb
@@ -1,10 +1,10 @@
 class LogInspector < Formula
   desc "AI-powered log analysis tool"
   homepage "https://github.com/hoaihuongbk/log-inspector"
-  version "0.1.0"
+  version "0.1.1"
 
   url "https://github.com/hoaihuongbk/log-inspector/releases/download/v#{version}/log-inspector"
-  sha256 "YOUR_BINARY_SHA256"
+  sha256 "8e4f4fe68756b2adabf87f8dd1b59e30deacd14ffb500c1d8dbb4651fc2c9f1e"
 
   def install
     bin.install "log-inspector"
